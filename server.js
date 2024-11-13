2f081e43d929cbdff36adaa34f7235172a0b2856
Ally Elvis Nzeyimana <allyelvis6569@gmail.com>
2024-11-14 00:45:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,22 +6,28 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+let currentBid = {
+  auctionId: 1,
+  highestBid: 0,
+  highestBidder: null,
+};
+
 io.on('connection', (socket) => {
-  console.log('User connected');
-
-  socket.on('joinAuction', (auctionId) => {
-    socket.join(auctionId);
-  });
-
-  socket.on('bid', (data) => {
-    io.to(data.auctionId).emit('newBid', data.bid);
+  console.log('New client connected');
+  socket.emit('currentBid', currentBid);  // Send initial bid data
+  
+  // Listen for new bids
+  socket.on('newBid', (bidData) => {
+    if (bidData.amount > currentBid.highestBid) {
+      currentBid.highestBid = bidData.amount;
+      currentBid.highestBidder = bidData.userId;
+      io.emit('currentBid', currentBid); // Broadcast updated bid to all clients
+    }
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('Client disconnected');
   });
 });
 
-server.listen(5000, () => {
-  console.log('Server running on port 5000');
-});
+server.listen(4000, () => console.log('Server is running on port 4000'));
